95b4268726da1b1c998bfc13f303f48422a6dd27
juyeopPark <juyeop.park@tictoccroc.com>
2022-11-01 22:01:03 +0900

    tyoeorm SYNCRONIZE false로 변경

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 function ormConfig(): TypeOrmModuleOptions {
   const commonConf = {
-    SYNCRONIZE: true,
+    SYNCRONIZE: false,
     ENTITIES: [__dirname + '/domain/*.entity{.ts,.js}'],
     MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
     MIGRATIONS_RUN: false,
